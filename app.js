0fb50b0bd82cb05c6cb5120568d8423a6a8bb92a
Billy Barnes <billy.barnes@us.abb.com>
2019-03-11 08:35:17 -0500

    Update app.js

--- app.js
+++ app.js
@@ -6,8 +6,22 @@ var app            = express();
 
 var port = 8001;
 
-require('./routes/livedata')(app, {});
+//require('./routes/livedata')(app, {});
+app.post('/livedata', (req, res) => {
+  // You'll create your note here.
+  res.send('Posting to livedata...' + req.body);
+  console.log(req.body);
+});
+
+app.get('/livedata', (req, res) => {
+ 
+ res.send('GET livedata...' + Date.now());
+  console.log('GET livedata');
+  
+ });
+
 
 app.listen(process.env.PORT || port, () => {
+  console.log("==========================================");
   console.log('We are live API on ' + port);
 });
